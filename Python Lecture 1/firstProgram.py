e172b24b74786e60923b45d4b707468031f44729
Bhavesh Joshi <159633229+ClassWithBhavesh@users.noreply.github.com>
2025-09-01 12:38:01 +0530

    first lecture notes uploaded

---
+++ Python Lecture 1/firstProgram.py
@@ -0,0 +1,73 @@
+# Variables in Python
+
+# userName1 = "Anurag"
+# userName2 = "Arpit"
+# userName3 = "Sagar"
+# userName4 = "Bhavesh"
+# print(userName1)
+# print(userName2)
+# print(userName3)
+# print(userName4)
+
+
+# Data-Types In Python - 1. primitive       2. non-primitive
+# 1. primitive - int, boolean, string, float
+# 2. non-primitive - list, set, dictionary, tupple
+
+
+
+# Primitive Data-Types
+
+# Integer Data-type
+# number = 25
+# print(number)
+# print(type(number))
+
+
+# Boolean Data-type
+# condition = True
+# print(condition)
+# print(type(condition))
+# condition = False
+# print(condition)
+# print(type(condition))
+
+
+# String Data-Type
+# userName4 = "Bhavesh"
+# print(userName4)
+# print(type(userName4))
+
+
+# Float Data-Type
+# userName4 = '2.36'
+# print(userName4)
+# print(type(userName4))
+
+
+
+
+
+# Non-Primitive Data-Types
+
+# List in Python
+# li = [10,20,30,40]
+# print(li)
+# print(type(li))
+
+# tup = (10,20,30,40)
+# print(tup)
+# print(type(tup))
+
+
+set = {10,20,30,40}
+print(set)
+print(type(set))
+
+
+dict = {
+    "user1": "Bhavesh",
+    "user2": "Anurag"
+}
+print(dict)
+print(type(dict))
