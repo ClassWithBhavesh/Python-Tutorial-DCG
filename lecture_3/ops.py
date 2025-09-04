e6e6b384fe2260426366b34471407bbf9d9182d8
Bhavesh Joshi <159633229+ClassWithBhavesh@users.noreply.github.com>
2025-09-04 19:36:16 +0530

    lecture notes uploaded

---
+++ lecture_3/ops.py
@@ -0,0 +1,64 @@
+# Operators in python
+
+# Arithmetic Operator
+# Assignment operator
+# Comparison Operator
+# Logical Operator
+# Membership operator
+# Identity operator
+# Bitwise Operator
+
+
+
+# Arithmetic operator : +, -, /, *,  %, //, **
+
+# num1 = 10
+# num2 = 52
+# print(num1 + num2)
+
+# num1 = 10
+# num2 = 52
+# print(num1 - num2 - num1)
+
+# num1 = 10
+# num2 = 9
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num2 % num1)
+
+
+# num1 = 10
+# num2 = 0
+
+# print(num2**num1)
+
+
+# Assignment operator - =, +=, -=, *=, /=, //=, **=, %=
+
+# num1 = 10
+# num3 = 98
+
+# num3 += num1    # num3 = num3 + num1
+
+# rest of the operator must be done by Bhavya and he admits it!
+
+
+
+# comparison Operator - ==, !=, <=, >=, <, >
+# num1 = 10
+# num3 = 98
+
+# print(num3 != num1)
+
+
+
+# Logical Operator - and, or, not
+
+# num1 = 10
+# num3 = 98
+# num6 = 64
+
+# print(num1 < num3 and num3 < num6)
+# print(not(num1 > num3 or num3 < num6))
+
---
+++ lecture_3/set.py
@@ -0,0 +1,79 @@
+# Set In Python
+
+# data_set = {10, True, False, "Dictionary"}
+# print(data_set)
+# print(type(data_set))
+
+# data_set.add("Arun")
+
+# print(data_set)
+
+# data_set.remove(10)
+# print(data_set)
+
+# data_set.discard("Arjun")
+# print(data_set)
+
+
+
+# Alternative ways to declare non-primitive data-types
+
+# set -
+# set = {}
+# print(type(set))
+
+# empty_set = set({10,20,50,99})
+# print(empty_set)
+
+# list - 
+# li = list(["Bhavesh", "Arjun"])
+# print(li)
+
+# tuple - 
+# tup = tuple(("Bhavesh", "Arjun"))
+# print(tup)
+
+
+# dictionary -
+# new_dict = dict({"key": "value", "key2""value2"))
+# print(new_dict)
+
+
+
+
+# Operations in Set 
+# Union in set - merge all the elements containing in a set
+# set1 = {1,2,3,4}
+# set2 = {3,4,5,6}
+
+# print(set1.union(set2))
+
+
+# intersection in set
+# set1 = {1,2,3,4}
+# set2 = {3,4,5,6}
+# set3 = {5,6,7,8}
+# newSet = set1.intersection(set2)
+# print(newSet)
+
+
+# difference im set
+
+# set1 = {1,2,3,4}
+# set2 = {3,4,5,6}
+# newSet = set2.difference(set1)
+
+# print(newSet)
+
+
+# symmetric in set
+# set1 = {1,2,3,4}
+# set2 = {3,4,5,6}
+# newSet = set2.symmetric_difference(set1)
+
+# print(newSet)
+
+
+
+
+
