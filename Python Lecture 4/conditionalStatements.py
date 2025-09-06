a028155b76ec83c1af3d606e73ac4456924d53f9
Bhavesh Joshi <159633229+ClassWithBhavesh@users.noreply.github.com>
2025-09-06 12:14:57 +0530

    lecture 4 updated

---
+++ Python Lecture 4/conditionalStatements.py
@@ -0,0 +1,51 @@
+# if | elif | else - if we want to perform any operations or functionalities on conditional basis then we use conditional statements
+
+
+
+# if - else :
+
+# dozen = 12
+# mango_count = 15
+
+# if(mango_count == dozen):
+#     print("mango_count are one dozen")
+# else:
+#     print("mango_count are not one dozen")
+
+
+
+# if - elif - else
+
+oneDozen = 12
+twoDozen = 24
+threeDozen = 36
+mango_count = 36
+
+
+# alternative methdod to resolve this problem statement 
+# if(mango_count == oneDozen or mango_count == twoDozen or mango_count == threeDozen):
+#     print("mango_count lies between our requirements")
+# else:
+#     print("mango_count doesn't lie between our reuqirements", mango_count)
+
+
+
+
+# if(mango_count == oneDozen):
+#     print("mango_count are one dozen")
+# elif(mango_count == twoDozen):
+#     print("mango_count are two dozen")
+# elif(mango_count == threeDozen):
+#     print("mango_count are three dozen")
+# else:
+#     print("mango_count doesn't lie between our reuqirements")
+
+
+# if(mango_count == oneDozen):
+#     print("mango_count are one dozen")
+# if(mango_count == twoDozen):
+#     print("mango_count are two dozen")
+# if(mango_count == threeDozen):
+#     print("mango_count are three dozen")
+# else:
+#     print("mango_count doesn't lie between our reuqirements")
---
+++ Python Lecture 4/opsPart2.py
@@ -0,0 +1,68 @@
+# Memebership operator | Identity Ops | Bitwise Ops
+
+
+# Membership Operator: in | not in
+
+
+# demo_list = [10,50,"String", True]
+
+# print("10" not in demo_list)
+# print("10" in demo_list)
+
+
+
+# Identity Ops : is | is not
+
+# demo_list = [10,50,"String", True]
+
+# print("10" is not demo_list)
+# print("10" is demo_list)
+
+
+
+
+
+# difference b/w membership & identity operator
+
+# mem_list = ["Anurag", "Mercedes", False, 50.36, 85]  # mem_list[0]
+# dup_mem_list = mem_list
+# demo_list = ["Anurag", "Mercedes", False, 50.36, 85] # demo_list[0]
+
+# print(mem_list not in dup_mem_list)
+# print(mem_list not in demo_list)
+
+
+# print(mem_list is not dup_mem_list)
+# print(mem_list is not demo_list)
+
+
+
+
+
+
+# Bitwise Operators - 0 = false     /      1 = true
+# & - AND  - 0:0 = 0 | 0:1 = 0 | 1:0 = 0 | 1:1 = 1
+# | - OR   - 0:0 = 0 | 0:1 = 1 | 1:0 = 1 | 1:1 = 1
+# ^ - XOR  - 0:0 = 0 | 0:1 = 1 | 1:0 = 1 | 1:1 = 0
+
+
+# num1 = 9
+# num2 = 10
+
+# print(bin(9))
+# print(bin(10))
+# print(num1 & num2)
+# print(num1 | num2)
+# print(num1 ^ num2)
+
+# 1001
+# 1010
+#  & - 1000
+#  | - 1011
+#  ^ - 0011
+
+
+
+
+
+
