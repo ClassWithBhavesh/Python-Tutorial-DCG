df0b914b549835ec5c9c1616deb32ce08674ee0c
Bhavesh Joshi <159633229+ClassWithBhavesh@users.noreply.github.com>
2025-09-11 19:19:41 +0530

    Add files via upload

---
+++ Python Lecture 5/loop.py
@@ -0,0 +1,53 @@
+# while loop -
+
+# vari = 1
+
+# while(vari <= 10):
+#     vari += 1
+#     print(vari)
+
+
+# for loop in python
+# vari = 1
+# n = 10
+# for vari in range(2, n, 2):
+#     print(vari)
+
+# print(vari)
+# li = []
+# print(li)
+
+# userName = "Aryan"
+# for vari in userName:
+#     li.append(vari)
+#     print(vari)
+
+
+# print(li)
+
+
+li = [20,50,"Bhavya", False]
+
+# for ind in li:
+#     print(ind)
+
+for ind, ele in enumerate(li):
+    print(ind)
+    print(ele)
+
+# for ind in range(20, False):
+#     print(ind)
+
+
+
+# dict - 
+
+# productDict = {
+#     "proName": "Mechanical Keyboard",
+#     "proPrice": 2000,
+#     "proCat": "Electronics"
+# }
+
+# for key, value in productDict.items():
+#     print(key, " : ", value)
+
