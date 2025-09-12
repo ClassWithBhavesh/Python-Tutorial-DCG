396cd62929ba60b8576051452721088abd9688ab
Bhavesh Joshi <159633229+ClassWithBhavesh@users.noreply.github.com>
2025-09-12 18:49:27 +0530

    python lecture 6 - Functions in Python

---
+++ Python Lecture 6/functions.py
@@ -0,0 +1,55 @@
+# Remaining concept - break & continue in loops
+# for i in range(1,10, 1):
+#     if i == 7:
+        # break         # break the execution loop
+        # continue      # skip the execution and will continue to next execution
+    # print(i)
+
+
+
+
+# Functions in Python - are a bunch of code (collection of code) wrapped in a single unit. they are reusable
+# To execute a function first we've to declare it and then call it
+
+# def perCalc():
+#     pass         # is used to skip the function declaration error
+
+
+
+# num1 = 10
+# num2 = 35
+# sum = num1 - num2
+# print(sum)
+
+
+
+# num1 = 19
+# num2 = 33
+
+# sum = num1 + num2
+# print(sum)
+
+# ele1 = int(input("Enter the number 1 : "))
+# ele2 = int(input("Enter the number 2 : "))
+
+
+# def sum(num1, num2):
+# def sum():
+    # num1 = 19
+    # num2 = 33
+    # ele1 = int(input("Enter the number 1 : "))
+    # ele2 = int(input("Enter the number 2 : "))
+
+    # sum = ele1 + ele2
+    # print(sum)
+    # sum = num1 + num2
+    # print(sum)
+
+# sum(ele1, ele2)
+# sum()
+# sum()
+# sum()
+
+
+
+
