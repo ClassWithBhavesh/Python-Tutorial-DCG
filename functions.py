6c4a956af1fce7e32b9ba18c65c7734c79b7738d
Bhavesh Joshi <159633229+ClassWithBhavesh@users.noreply.github.com>
2025-09-18 12:44:04 +0530

    function and recursion notes and questions updated

---
+++ functions.py
@@ -0,0 +1,48 @@
+# Remaining stuffs - 
+
+# def depositAmount(m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11 = 1000, m12= 1000):
+#     totDepoAmount = m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + m10 + m11 + m12
+#     print(totDepoAmount)
+
+# depositAmount(10,20,30,60,50,40,80,20,90,20)
+
+
+
+
+
+
+# function using tuple parameter (*)
+
+# tup = (10,20,30,60,50,40,80,20,90,20)
+# def depositAmount(*depoAm):
+#     print(depoAm)
+#     sum = 0
+#     for i in depoAm:
+#         sum += i
+        
+#     print(sum)
+
+# depositAmount(*tup)
+# depositAmount(10,20,30,60,50,40,80,20,90,20)
+
+
+# proDict = {
+#     "proName": "Macbook",
+#     "proPrice": "2.3L",
+#     "proVarient": "m4",
+#     "proColor": "NavyBlue"
+# }
+
+# def proDetails(**productDetails):
+#     print(productDetails)
+    # proValues = productDetails.items()
+    # for i in proValues:
+    #     print(i)
+
+# proDetails(**proDict)
+
+
+
+
+
+
---
+++ recursion.py
@@ -0,0 +1,26 @@
+# a function to calculate marks in percentage and the Grade of each students of Xth class
+
+# Factorial number : 5 & 4
+# 5 * 4 * 3 * 2 * 1 = 120
+# 4 * 3 * 2 * 1     = 24
+
+# n-0 * n-1 * n-2 * n-3 * n-4
+
+# def fact(n):
+#     if n == 1 or n == 0:
+#         return 1
+#     else:
+#         return n * fact(n-1)
+    
+# factorialNum = fact(10)
+# print(factorialNum)
+
+
+
+
+
+
+
+# fibonacci series - 10
+# 1 1 2 3 5 8 13 21 34 55
+
