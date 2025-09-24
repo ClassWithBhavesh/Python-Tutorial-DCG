4f668e5e513dc0b0f1ec2b03c14ee4ad8f8b3c38
Bhavesh Joshi <classwithbhavesh@gmail.com>
2025-09-24 20:15:18 +0530

    numpy overview covered

---
+++ Lecture 8 - NumPy Overview/numpyOverview.py
@@ -0,0 +1,38 @@
+# import numpy
+# import time
+
+# Comaprsion between Python List & NumPy Array - 
+
+# startTime = time.time()
+# for i in range(1, 11):
+#     print(i**2)
+# endTime = time.time()
+# print("Time consumed to build a python list - ", (endTime - startTime))
+
+
+
+# startTime1 = time.time()
+# arr = numpy.arange(1,11)
+# newArr = arr**2
+# # print(arr)
+# endTime1 = time.time()
+# print("Time consumed to build a python list - ", (endTime1 - startTime1))
+
+
+
+
+
+# numpy array - 
+
+# import numpy as py
+
+# li = [10,20,30,60,30]
+# print(li)
+# arr = py.array(li)
+# print(arr)
+
+
+# arr = py.array([[[10,20,30], [1,2,3], [0,0,0]], [[10,20,30],[1,2,3], [0,0,0]]])
+# print(arr)
+# print(arr.ndim)
+
---
+++ Lecture 8 - NumPy Overview/reversedStringDoubt.py
@@ -0,0 +1,21 @@
+# name_str = "Bhavyashree"
+
+# name_list = list(name_str)
+# print(name_list)
+# liLen = len(name_list)
+# print(liLen)
+# li = []
+# print(li)
+
+# for i in range(liLen-1, -1, -1):
+#     li.append(name_list[i])
+
+# reversedString = "".join(li)
+# print(name_str)
+# print(reversedString)
+# for i in name_list:
+#     print(i)
+
+
+# for i in range(0, 10):
+#     print(i)
