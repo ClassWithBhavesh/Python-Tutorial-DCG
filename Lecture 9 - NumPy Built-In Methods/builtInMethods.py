59ec5d60ac121a6c279bf997492d207048501b29
Bhavesh Joshi <classwithbhavesh@gmail.com>
2025-09-25 19:32:57 +0530

    typeCasting & data types in numpy

---
+++ Lecture 9 - NumPy Built-In Methods/builtInMethods.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+# arr = np.array([1,10,1])
+# print(arr)
+
+# new_arr = np.array(arr)
+# print(new_arr)
+
+
+# arr = np.arange(10, 0, 3)
+# print(arr)
+
+# twoDArr = np.array([[1,2,3], [10,20, 2], [20, 22, 65]])
+# print(twoDArr)
+# print(twoDArr.ndim)
+
+
+
+
+
+# Data-Types in NumPy | Type-Casting in NumPy - 
+
+# num = 2
+
+
+# num = np.array([20, False, True, 10, 1.3])
+# print(num)
+# print(num.dtype)
+# num2 = num.astype(np.int32)
+# print(num.dtype)
+# print(num2)
+# print(num2.dtype)
+
+
+# num = np.array(["20", False, True, "10", 1.3], dtype=np.int8)
+# print(num)
+# print(num.dtype)
+# num[0] = 100
+# print(num)
+# print(num.dtype)
+# print(type(num))
+
+
+
+
+
+
+# arr = np.array([10,20,30,60])
+# print(arr)
+
+# new_arr = np.linspace(1, 10, 10)
+# print(new_arr)
+
+
+
