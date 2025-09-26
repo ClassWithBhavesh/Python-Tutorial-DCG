98f19c3691806dd2f43ad5361ccafa057ee78b5e
Bhavesh Joshi <classwithbhavesh@gmail.com>
2025-09-26 20:23:23 +0530

    Indexing and Slicing covered

---
+++ Lecture 10 - Indexing & Slicing/indexing&slicing.py
@@ -0,0 +1,101 @@
+import numpy as np
+
+# arr = np.array([10,20,30,60])
+# print(arr)
+
+# negArr = np.arange(-10, 0)
+# print(negArr)
+
+# eqArr = np.linspace(-10, -1, 5)
+# print(eqArr)
+
+# orgArr = np.array([10,20,30,60], dtype=np.int8)
+# print(orgArr.dtype)
+
+# copyArr = orgArr.astype(np.int32)
+# print(copyArr.dtype)
+
+# print(orgArr.dtype)
+
+
+
+
+# ndim array - 
+# nArr = np.array([[10,20,50], ["a", "b", "C"]])
+# print(nArr.ndim)
+# print(nArr)
+
+
+
+# Indexing & Slicing on NumPy Arrays
+# threeDArr = np.array([[[[[10,20,30]]]]])
+# print(threeDArr[0][0][0][0][1])
+
+# threeDArr = np.array([[[10,20,30],[1,2,3], [100,200,300]], [[10,20,30], [100,200,300], [1,2,3]], ["A", "B", "C"], ["a", "b", "c"], ["@", "$", "*"]])
+# print(threeDArr.ndim)
+
+# print(threeDArr[2][2][1])
+# print(threeDArr[2][2][1])
+# print(threeDArr[1][0][2])
+# print(threeDArr[0][1][2])
+# print(threeDArr[0][0][2])
+# print(threeDArr[0][1][1])  --> 2
+
+
+
+# arr = np.array([7,8,9,6,5,4,3,2,1])
+# print(arr)
+
+
+
+
+
+# print(arr[-7])
+# new_arr = arr[-6:len(arr)]
+# print(new_arr)
+
+# new_arr = arr[0:7:2]
+# print(new_arr)
+
+# new_arr = arr[:]
+# print(new_arr)
+# new_arr[2] = 50
+# print(new_arr)
+# print(arr)
+
+
+# new_arr = arr[0:len(arr)]
+# print(new_arr)
+# new_arr[2] = 50
+# print(new_arr)
+# print(arr)
+
+
+
+# matrix = np.zeros([2,3, 4, 5], dtype=np.int64)
+# print(matrix.dtype)
+# print(matrix)
+
+# matrix = np.ones([2,3, 4, 5])
+# print(matrix.dtype)
+# print(matrix)
+
+# matrix = np.full([2,3], True, dtype=np.str_)
+# print(matrix.dtype)
+# print(matrix)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
