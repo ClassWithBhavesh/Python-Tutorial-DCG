e565d74b05f20385d117a5b5ebe93d78696b40c9
Bhavesh Joshi <classwithbhavesh@gmail.com>
2025-09-30 20:28:55 +0530

    Add files via upload

---
+++ Python Lecture 11 - built-in methods/builtinFunc.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+# randNum = np.random.rand(3,2)*101
+# print(randNum)
+
+# intArr = np.array(randNum, dtype=np.int64)
+# print(intArr)
+
+# intArr[0][0] = 100
+
+# print(randNum)
+# print(intArr)
+
+# nArr = np.random.randn(2,3)
+# print(nArr)
+
+# arrShape = np.shape(nArr)
+# arrShape = np.size(nArr)
+# print(arrShape)
+
+
+
+# to generate an array of a random integer values
+# intArr = np.random.randint(2,200, size= (2,3,6))
+# print(intArr.shape)
+
+
+# intArr = np.arange(200,236)
+# print(intArr)
+
+# Re-Shaping of an array 
+# reshapedArr = intArr.reshape(4,3,3)
+# print(reshapedArr.itemsize)
+# print(reshapedArr)
+
+
+
+
+
+# flattening of an array 
+# flatArr = reshapedArr.ravel()
+# print(flatArr)
+# flatArr[0] = 25
+# print(flatArr)
+# print(reshapedArr)
+
+# flatArr2 = reshapedArr.flatten()
+# print(flatArr2)
+# flatArr2[0] = 26
+# print(flatArr2)
+# print(reshapedArr)
+
+
+
